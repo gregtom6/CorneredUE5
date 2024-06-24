ba9fbcac81f5c9b161f3891245e1d2ad9f42d62c
gregtom6 <gregtom6@gmail.com>
2024-06-24 11:33:46 +0200

    init commit

---
+++ Cornered/Source/Cornered/Cornered.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Cornered.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Cornered, "Cornered" );

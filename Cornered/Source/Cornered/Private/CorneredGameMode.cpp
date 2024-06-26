35550717d37e4866e1a30fc9d0af51b9ded94701
gregtom6 <gregtom6@gmail.com>
2024-06-26 13:33:24 +0200

    creating and setting Game Mode and setting BP_PlayerCharacter in it as default pawn

---
+++ Cornered/Source/Cornered/Private/CorneredGameMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CorneredGameMode.h"
+
+ACorneredGameMode::ACorneredGameMode()
+{
+	static ConstructorHelpers::FClassFinder<APawn>PlayerPawnBPClass(TEXT("/Game/Data/Blueprints/BP_PlayerCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
+
---
+++ Cornered/Source/Cornered/Public/CorneredGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CorneredGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORNERED_API ACorneredGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+		ACorneredGameMode();
+};

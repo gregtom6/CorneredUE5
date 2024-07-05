a34ccc46ae24ed854f056150298ad409005fcd64
gregtom6 <gregtom6@gmail.com>
2024-07-05 14:17:26 +0200

    progressing with the level - making roofs and walls along corridors

--- Cornered/enc_temp_folder/8ce3b01219dc94ded0c3cecc9fbe678e/PlayerWeapon.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PlayerWeapon.h"
-
-EItemType UPlayerWeapon::GetEquippedWeapon() const {
-	return EItemType::Pistol;
-}

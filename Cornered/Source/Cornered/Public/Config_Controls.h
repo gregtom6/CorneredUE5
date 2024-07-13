b12517bd02a8358ceadbe9d936fc95164eea85be
gregtom6 <gregtom6@gmail.com>
2024-07-13 21:22:45 +0200

    basic files (gamemode, level, data assets, hud class, blueprints) created for main menu

---
+++ Cornered/Source/Cornered/Public/Config_Controls.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Config_Controls.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORNERED_API UConfig_Controls : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
